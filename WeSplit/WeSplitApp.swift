7dbddf2402c150f3b3218a7b3748e5ccb393d65b
rdgronii <rgroner2@gmail.com>
2023-10-12 11:52:54 -0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Ricky David Groner II on 10/12/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
